f15c152d9ad562d64d662cad21caea3ad061f407
gowtham598/demo1 <g80993235@gmail.com>
2026-02-02 10:02:40 +0530

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  base: "/demo1/", // MUST match your repo name
+  base: "/ReactGitDeployDemo/", //YourRepoName
 })
